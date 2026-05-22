74b667a0ea62190e8da800e79c8f562b3c8f6502
Maj-Britt Deigaard <deigaardm@gmail.com>
2026-05-22 14:32:45 +0200

    trying to make SSE work

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,5 +4,6 @@
 class ApiService {
   // Use http://10.0.2.2:3000 for Android emulator.
   // Use http://localhost:3000 for Flutter web or desktop.
-  static const String baseUrl = 'http://localhost:3000';
+  //static const String baseUrl = 'http://localhost:3000';
+  static const String baseUrl = 'http://10.0.2.2:3000';
 }
